1672b48638d366e0a1be769188b8c90c55ff3c7e
BjoKaSH <googlelogin@bjoern-kahl.de>
2012-08-19 22:55:36 +0200

    Cleandup packaging script, added post-install step to fix permissions.

--- support/release.sh
+++ support/release.sh
@@ -18,24 +18,35 @@ then
 fi
 
 USER=`whoami`
-GROUP=`id -n -g`
+GROUP=`id -gn`
 
 (
 mkdir -p ${DIR}/../${BUILD}
 rm -rf ${DIR}/../${BUILD}/ZFS105
 rm -rf ${DIR}/../${BUILD}/ZFS106
 cp -R ${DIR}/MacZFS.pmdoc ${DIR}/../${BUILD}
+cp -R ${DIR}/post-install.sh ${DIR}/../${BUILD}
 sed -i~  -e "s/0\\.0\\.0/${VERSION}/g" ${DIR}/../${BUILD}/MacZFS.pmdoc/*
 
 cd ${DIR}/..
 xcodebuild -sdk macosx10.5 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} INSTALL_GROUP=${GROUP} SYMROOT=${BUILD}/${CONFIG}105 DSTROOT=${BUILD}/ZFS105 || exit 2
-
+chmod -Rv u+w,go-w,a+r ${BUILD}/ZFS105
+echo "explicitly setting group and ownership, enter sudo password if prompted"
+sudo chown -Rv root:wheel ${BUILD}/ZFS105
 # No point in building on 10.5, it'll just do the same thing again
 if [ "`sysctl -b kern.osrelease`" != "9.8.0" ] 
 then
 	xcodebuild -sdk macosx10.6 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} INSTALL_GROUP=${GROUP} SYMROOT=${BUILD}/${CONFIG}106 DSTROOT=${BUILD}/ZFS106 || exit 3
+	chmod -R u+w,go-w,a+r ${BUILD}/ZFS106
+	sudo chown -Rv root:wheel ${BUILD}/ZFS106
 fi
 
 cd ${BUILD}
-packagemaker --doc MacZFS.pmdoc --version ${VERSION} --title "Mac ZFS ${VERSION}" --out MacZFS-${VERSION}.pkg --target 10.5 || exit 4
+# run packagemaker and save exit code, do NOT abort! We must fix permissions first.
+packagemaker --doc MacZFS.pmdoc --version ${VERSION} --title "Mac ZFS ${VERSION}" --out MacZFS-${VERSION}.pkg --target 10.5
+err=$?
+sudo chown -R ${USER}:${GROUP} ZFS10?
+if [ ${err} -ne 0 ] ; then
+	exit 4
+fi 
 )
