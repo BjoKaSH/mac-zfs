eac409ccac7b73f41256f3dfd11695ff1e357680
BjoKaSH <googlelogin@bjoern-kahl.de>
2013-12-01 20:18:32 +0100

    Moved common initialization code into new function "tests_std_setup".

--- test-scripts/run-tests-basic.sh
+++ test-scripts/run-tests-basic.sh
@@ -1,67 +1,41 @@
 #! /bin/bash
 
-set -x -v
-export LC_ALL=C
+#set -x -v
 
-# load configuration, if present
-#
-# set defaults
-poolbase=pool_$(date +%s)_$$
-has_fstest=0
-genrand_bin=./test-scripts/genrand
-#
-# check for local config file
-conf=maczfs-tests.conf
-if [ "${1:-}" == "-C" ] ; then
-    conf="${2:-}"
-    if [ ! -f "${conf}" ] ; then
-        echo "Config file '${conf}' not readable."
-        exit 1
-    fi
-elif [ "${1:-}" == "--help" ] ; then
-    echo "$0 [ -C conf-file ] "
-    exit 1
-fi
-
-if [ -f ${conf} ] ; then
-    source ${conf}
-    # make sure diskstore is set, otherwise bad things will happen
-    if [ ! -d "${diskstore}" ] ; then
-        echo "diskstore not set. Abort."
-        exit 1
-    fi
-else
-    diskstore=$(mktemp -d -t diskstore_)
-fi
-
-stop_on_fail=1
-
-if [ ! -z "${tests_tmpdir}" ] ; then
-    export TMPDIR=${tests_tmpdir}
-else
-    tests_tmpdir=$(mktemp -d -t tests_maczfs_)
-    export TMPDIR=${tests_tmpdir}
-fi
-
-if [ -z "${tests_logdir}" ] ; then
-    tests_logdir=$(mktemp -d -t test_logs_maczfs_)
-fi
-
-if [ ! -x "${genrand_bin}" ] ; then
-    echo "Error: random number generator '' not found."
-    echo "Did you compiled it? ('gcc -o genrand -O3 genrand.c' in the support folder.)"
-fi
-
-# initialize random datas generator
-genrand_state=${tests_logdir}/randstate.txt
-${genrand_bin} -s 13446 -S ${genrand_state}
-
-tests_func_init_done=1
+tests_func_init_done=0
 
 # load various helper functions
-source ./test-scripts/tests-functions.sh
+if [ -z "${test_scripts_dir:-}" ] ; then
+    test_scripts_dir=."$(dirname "$0")"
+fi
+test_scripts_lib=${test_scripts_dir}/tests-functions.sh
+if [ ! -f "${test_scripts_lib}" ] ; then
+    read -p  "test functions 'tests-functions.sh' not found.  Enter path: "  test_scripts_lib
+    test_scripts_dir="$(dirname "${test_scripts_lib}")"
+fi
+if [ ! -f "${test_scripts_dir}/tests-functions.sh" ] ; then
+    echo "Need 'tests-functions.sh' but not found at '${test_scripts_lib}'.  Abort."
+    exit 1
+fi
+source ${test_scripts_dir}/tests-functions.sh
 
-trap "interact '(err)'" EXIT
+# define arguments specific to this set of tests
+extra_args_help_short=" [ --run_fstest 1/0 ]  [ --fstest_suite path-to-fstest-script ] "
+extra_args_help_long="   --run_fstest 1/0   run the fstest suite if set to 1\n   --fstest_suite path  path to script that executes fstest in the current directory"
+extra_args="--run_fstest:has_fstest --fstest_suite:fstest_suite"
+
+# defaults for above arguments
+has_fstest=0
+fstest_suite=run-fstest.sh
+
+# initialize test system and parse arguments
+tests_std_setup
+
+tottests=82
+
+if [ ${has_fstest} -eq 1 ] ; then
+    tottests=$(($tottest+1))
+fi
 
 # Test sequence:
 # - create single-disk pool in default config, using disk-based vdev "vd1"
@@ -71,20 +45,6 @@ trap "interact '(err)'" EXIT
 #   - verify it can be exported
 #   - verify it can be reimported and auto-mounts
 
-cleanup=0
-failcnt=0
-okcnt=0
-subfailcnt=0
-subokcnt=0
-cursubtest=0
-tottests=82
-curtest=0
-
-if [ ${has_fstest} -eq 1 ] ; then
-    tottests=$(($tottest+1))
-fi
-
-run_abort 0 mkdir ${diskstore}/dev
 
 run_ret 0 "Create disk vd1" make_disk 5 vd1 8
 attach_disk vd1
@@ -473,7 +433,5 @@ run_ret 0 "Destroying pool p1" destroy_pool p1
 
 #
 # Done
-
-trap - EXIT
 
 # End
